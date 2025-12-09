defa4000d9e4728cab6a9e457187895465eca583
Your Name <keucao@hotmail.com>
2025-12-08 21:58:04 -0300

    Algumas correções

--- variables/backendVariables/11-volumeEsfera/index.js
+++ variables/backendVariables/11-volumeEsfera/index.js
@@ -1,9 +1,20 @@
-const diametro = 6;
-const raio = diametro / 2;
-const pi = Math.PI;
+const d = 6;
+const r = d / 2;
 
-const volume = (4/3) * pi * (raio ** 3);
+const v = 4 / 3 * r ** 3;
 
-const volumeArredondado = volume.toFixed(2);
+console.log(`O volume da esfera é ${v} PI ou ${(v * Math.PI).toFixed(2)}`);
 
-console.log(`O volume de uma esfera de raio ${raio} é ${volumeArredondado} PI`)
+
+
+
+
+// const diametro = 6;
+// const raio = diametro / 2;
+// const pi = Math.PI;
+
+// const volume = (4/3) * pi * (raio ** 3);
+
+// const volumeArredondado = volume.toFixed(2);
+
+// console.log(`O volume de uma esfera de raio ${raio} é ${volumeArredondado} `)
