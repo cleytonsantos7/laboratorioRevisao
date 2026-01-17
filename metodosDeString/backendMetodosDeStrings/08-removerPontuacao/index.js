502cadbdd32036de8178bb7b4e98c595c8a49f32
Cleyton Santos <135157379+cleytonsantos7@users.noreply.github.com>
2026-01-17 00:10:46 -0300

    Adicionado outra forma de resolução do exercicio 8 do backend de metodos de string

--- metodosDeString/backendMetodosDeStrings/08-removerPontuacao/index.js
+++ metodosDeString/backendMetodosDeStrings/08-removerPontuacao/index.js
@@ -1,8 +1,20 @@
 const cpf = "011.022.033-44";
 
-function removerPontuacao(documento) {
-    const resultado = documento.replace(/\D/g, '');
-    console.log(resultado);
+const removerPontuacao = (numero) => {
+    let numeros = numero.replace('-', '').replace('/', '').split('.');
+    let numeroLimpo = '';
+
+    for (const item of numeros) {
+        numeroLimpo += item;
+    }
+
+    console.log(numeroLimpo);
 }
+
+
+// function removerPontuacao(documento) {
+//     const resultado = documento.replace(/\D/g, '');
+//     console.log(resultado);
+// }
 
 removerPontuacao(cpf);
