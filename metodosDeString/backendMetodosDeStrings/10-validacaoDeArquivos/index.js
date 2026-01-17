d59a3bbe8ff4c7bb4d5e7e57d6bdf54cbf732f71
Cleyton Santos <135157379+cleytonsantos7@users.noreply.github.com>
2026-01-17 00:10:47 -0300

    Adicionado outra forma de resolução do exercicio 10 do backend de metodos de string

--- metodosDeString/backendMetodosDeStrings/10-validacaoDeArquivos/index.js
+++ metodosDeString/backendMetodosDeStrings/10-validacaoDeArquivos/index.js
@@ -1,17 +1,27 @@
 const nomeArquivo = 'Foto da Familia.pdf';
 
-function validarArquivo(nomeArquivo) {
-    const extensoesPermitidas = ['.jpg', '.jpeg', '.gif', '.png'];
+const validarArquivo = (arquivo) => {
+    const extensao = arquivo.slice(arquivo.lastIndexOf('.') + 1);
 
-    const extensao = nomeArquivo
-        .slice(nomeArquivo.lastIndexOf('.'))
-        .toLowerCase();
-
-    if (extensoesPermitidas.includes(extensao)) {
+    if (extensao === 'jpg' || extensao === 'jpeg' || extensao === 'git' || extensao === 'png') {
         console.log('Arquivo válido');
     } else {
         console.log('Arquivo inválido');
     }
 }
+
+// function validarArquivo(nomeArquivo) {
+//     const extensoesPermitidas = ['.jpg', '.jpeg', '.gif', '.png'];
+
+//     const extensao = nomeArquivo
+//         .slice(nomeArquivo.lastIndexOf('.'))
+//         .toLowerCase();
+
+//     if (extensoesPermitidas.includes(extensao)) {
+//         console.log('Arquivo válido');
+//     } else {
+//         console.log('Arquivo inválido');
+//     }
+// }
 
 validarArquivo(nomeArquivo);
