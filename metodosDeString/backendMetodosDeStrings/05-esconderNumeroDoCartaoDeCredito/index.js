0711820b75cd466364460327c4133537ec58ec8c
Cleyton Santos <135157379+cleytonsantos7@users.noreply.github.com>
2026-01-17 00:10:46 -0300

    Adicionado outra forma de resolução do exercicio 5 do backend de metodos de string

--- metodosDeString/backendMetodosDeStrings/05-esconderNumeroDoCartaoDeCredito/index.js
+++ metodosDeString/backendMetodosDeStrings/05-esconderNumeroDoCartaoDeCredito/index.js
@@ -1,15 +1,23 @@
 const numeroCartao = '1111222233334444';
 
-function formatarCartao(numeroCartao) {
-  const clean = String(numeroCartao).replace(/\D/g, '');
-  if (clean.length < 8) return 'Número de cartão inválido';
+const numerosIniciais = numeroCartao.slice(0, 4);
+const numerosFinais = numeroCartao.slice(12);
 
-  const primeiro = clean.slice(0, 4);
-  const ultimo = clean.slice(-4);
-  const ocultos = '*'.repeat(Math.max(0, clean.length - 8));
-  const masked = `${primeiro}${ocultos}${ultimo}`;
+const numeroCartaoFormatado = `${numerosIniciais.padEnd(8, '*')}${numerosFinais.padStart(8, '*')}`
 
-  return masked.replace(/(.{4})(?=.)/g, '$1 ');
-}
+console.log(numeroCartaoFormatado)
+
+
+// function formatarCartao(numeroCartao) {
+//   const clean = String(numeroCartao).replace(/\D/g, '');
+//   if (clean.length < 8) return 'Número de cartão inválido';
+
+//   const primeiro = clean.slice(0, 4);
+//   const ultimo = clean.slice(-4);
+//   const ocultos = '*'.repeat(Math.max(0, clean.length - 8));
+//   const masked = `${primeiro}${ocultos}${ultimo}`;
+
+//   return masked.replace(/(.{4})(?=.)/g, '$1 ');
+// }
   
-  console.log(formatarCartao(numeroCartao));
+//   console.log(formatarCartao(numeroCartao));
