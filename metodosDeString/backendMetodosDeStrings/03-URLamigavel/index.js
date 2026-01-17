7bf83014682c7516027b615053bd550d6e9b6a4a
Cleyton Santos <135157379+cleytonsantos7@users.noreply.github.com>
2026-01-17 00:10:45 -0300

    Adicionado outra formula de resolução do exercicio 3 do backend de metodos de string

--- metodosDeString/backendMetodosDeStrings/03-URLamigavel/index.js
+++ metodosDeString/backendMetodosDeStrings/03-URLamigavel/index.js
@@ -1,16 +1,38 @@
 const texto = "Aprenda programar do zero na Cubos Academy";
 
-function textoParaURL(texto) {
-  if (typeof texto !== 'string') texto = String(texto);
+const transformarTextoEmUrl = (texto) => {
+  let url = '';
 
-  return texto
-    .toLowerCase()
-    .normalize('NFD').replace(/[\u0300-\u036f]/g, '') // remove acentos
-    .replace(/[^a-z0-9\s-]/g, '')                     // permite letras, números, espaços e hífen
-    .trim()
-    .replace(/\s+/g, '-')                             // espaços → hífen
-    .replace(/-+/g, '-')                              // colapsa hífens múltiplos
-    .replace(/^-+|-+$/g, '');                         // remove hífens nas bordas
+  const arrayPalavras = texto.split(' ');
+
+  for (let i = 0; i < arrayPalavras.length; i++) {
+    if (i > 0) {
+      url += `-${arrayPalavras[i]}`;
+  } else {
+    url += `${arrayPalavras[i]}`;
+  }
+  }
+  console.log(url.toLowerCase());
 }
 
-console.log(textoParaURL(texto));
+transformarTextoEmUrl(texto)
+
+
+
+
+// const texto = "Aprenda programar do zero na Cubos Academy";
+
+// function textoParaURL(texto) {
+//   if (typeof texto !== 'string') texto = String(texto);
+
+//   return texto
+//     .toLowerCase()
+//     .normalize('NFD').replace(/[\u0300-\u036f]/g, '') // remove acentos
+//     .replace(/[^a-z0-9\s-]/g, '')                     // permite letras, números, espaços e hífen
+//     .trim()
+//     .replace(/\s+/g, '-')                             // espaços → hífen
+//     .replace(/-+/g, '-')                              // colapsa hífens múltiplos
+//     .replace(/^-+|-+$/g, '');                         // remove hífens nas bordas
+// }
+
+// console.log(textoParaURL(texto));
