f2d8e8519846c59f948ddfe3af3b0cfba9070886
Cleyton Santos <135157379+cleytonsantos7@users.noreply.github.com>
2026-01-17 00:10:46 -0300

    Adicionado outra forma de resolução do exercicio 7 do backend de metodos de string

--- metodosDeString/backendMetodosDeStrings/07-validacaoDeEmail/index.js
+++ metodosDeString/backendMetodosDeStrings/07-validacaoDeEmail/index.js
@@ -1,17 +1,38 @@
 const email = "aluno@cubos.academy";
 
-function validarEmail(email) {
-    if (!email.includes('@')) return 'E-mail inválido';
+const validarEmail = (email) => {
+    let emailValido = true;
 
-    const [usuario, dominio] = email.split('@');
+    if (email.indexOf('@') < 0){
+        emailValido = false;
+    } else if (email.indexOf('.') < 0) {
+        emailValido = false;
+    } else if (email.indexOf('.') === 0) {
+        emailValido = false;
+    } else if (email.lastIndexOf('.') === email.length) {
+        emailValido = false;
+    }
 
-    if (!usuario || !dominio) return 'E-mail inválido';
-    if (usuario.startsWith('.') || usuario.endsWith('.')) return 'E-mail inválido';
-    if (!dominio.includes('.')) return 'E-mail inválido';
-    if (dominio.startsWith('.') || dominio.endsWith('.')) return 'E-mail inválido';
-
-    return 'E-mail válido';
+    if (emailValido){
+        console.log('E-mail válido')
+    } else {
+        console.log('E-mail inválido')
+    }
 }
 
 
-console.log(validarEmail(email));
+// function validarEmail(email) {
+//     if (!email.includes('@')) return 'E-mail inválido';
+
+//     const [usuario, dominio] = email.split('@');
+
+//     if (!usuario || !dominio) return 'E-mail inválido';
+//     if (usuario.startsWith('.') || usuario.endsWith('.')) return 'E-mail inválido';
+//     if (!dominio.includes('.')) return 'E-mail inválido';
+//     if (dominio.startsWith('.') || dominio.endsWith('.')) return 'E-mail inválido';
+
+//     return 'E-mail válido';
+// }
+
+
+// console.log(validarEmail(email));
