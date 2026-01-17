1ed2e5f28c39b3ca2cce100566e6d18a96e24840
Cleyton Santos <135157379+cleytonsantos7@users.noreply.github.com>
2026-01-17 00:10:47 -0300

    Adicionado outra forma de resolução do exercicio 9 do backend de metodos de string

--- metodosDeString/backendMetodosDeStrings/09-geradorDeNickname/index.js
+++ metodosDeString/backendMetodosDeStrings/09-geradorDeNickname/index.js
@@ -1,9 +1,22 @@
 const nome = 'Guido Cerqueira';
 
-function gerarNickname(nome) {
-    const nickname = `@${nome.replace(/\s/g, '').toLowerCase().substring(0, 12)}`;
-    console.log(nickname);
-};
+const gerarNickname = (nome) => {
+    const nomes = nome.trim().toLowerCase().split(' ');
+    let nomeSemEspaco = '';
+
+    for (const item of nomes) {
+        nomeSemEspaco += item;
+    }
+    
+    const nickname = nomeSemEspaco.slice(0, 12);
+    console.log(`@${nickname}`)
+}
+
+
+// function gerarNickname(nome) {
+//     const nickname = `@${nome.replace(/\s/g, '').toLowerCase().substring(0, 12)}`;
+//     console.log(nickname);
+// };
 
 
 gerarNickname(nome);
