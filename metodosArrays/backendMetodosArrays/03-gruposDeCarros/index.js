a21477bee7ec0cd60c6a153788f70d137dc6defa
Cleyton Santos <135157379+cleytonsantos7@users.noreply.github.com>
2026-01-19 22:19:26 -0300

    Adicionada resolução do exercicio 3 da pasta backendMetodosArrays

---
+++ metodosArrays/backendMetodosArrays/03-gruposDeCarros/index.js
@@ -0,0 +1,11 @@
+const nomes = ['Ford Ká', 'Ranger', 'Hilux', 'Corola', 'Fusca', 'Chevete', 'Brasilia'];
+const posicao = 3;
+
+function declararPosicao(nomes, posicao) {
+    const nomes3posicoes = nomes.slice(posicao, posicao + 3);
+
+    console.log(nomes3posicoes)
+}
+
+declararPosicao(nomes, posicao)
+
