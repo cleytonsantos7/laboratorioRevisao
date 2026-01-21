edcc94e652b6c1243451c8093d622160ce6336de
Cleyton Santos <135157379+cleytonsantos7@users.noreply.github.com>
2026-01-21 16:52:08 -0300

    Criação da pasta metodosArrays2, e adição do conceito de callback

---
+++ metodosArrays2/callbackSetTimeout.js
@@ -0,0 +1,13 @@
+const imprimirNome = () => {
+    console.log('Guido Cerqueira')
+}
+
+// setTimeout(imprimirNome, 5000)
+
+// setTimeout (() => {
+//     console.log('Cubos Academy')
+// }, 2000)
+
+setTimeout (function () {
+    console.log('Cubos Academy')
+})
