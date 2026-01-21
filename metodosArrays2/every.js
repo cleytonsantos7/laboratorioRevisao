06faf9f37eee490015d7a755ae7c212d807bcd2f
Cleyton Santos <135157379+cleytonsantos7@users.noreply.github.com>
2026-01-21 17:19:12 -0300

    Adição de exemplos praticos do uso do every

--- metodosArrays2/every.js
+++ metodosArrays2/every.js
@@ -1,7 +1,17 @@
 const numeros = [1, 2, 3, 4];
 
-numeros.every((elementoAtual) => {
-    return elementoAtual <5;
+const resultado = numeros.every((numero) => {
+    return numero <5;
 });
 
 //return true
+
+const frutas = ['abacaxi', 'manga', 'melancia'];
+
+const resultado1 = frutas.every(function(elementoAtual){
+    return elementoAtual !== 'manga';
+});
+
+//return false
+
+console.log(resultado, resultado1);
