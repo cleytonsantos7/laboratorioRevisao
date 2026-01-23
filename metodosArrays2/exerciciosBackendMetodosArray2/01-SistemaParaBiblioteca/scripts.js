7944af753e0af90ad790753cdc0d05744b69952f
Cleyton Santos <135157379+cleytonsantos7@users.noreply.github.com>
2026-01-23 17:38:22 -0300

    Adição da resolução do exercicio 1, da pasta exerciciosBackendMetodosArrays2

---
+++ metodosArrays2/exerciciosBackendMetodosArray2/01-SistemaParaBiblioteca/scripts.js
@@ -0,0 +1,21 @@
+const livros = ['Guerra e Paz', 'A Montanha Mágica', 'Cem Anos de Solidão', 'Dom Quixote', 'A Divina Comédia'];
+const nomeDoLivro = "Dom Quixote";
+
+const posicaoDoLivro = livros.findIndex((livro) => {
+    return livro === nomeDoLivro
+});
+
+console.log(posicaoDoLivro + 1);
+
+
+
+// const livros = ['Guerra e Paz', 'A Montanha Mágica', 'Cem Anos de Solidão', 'Dom Quixote', 'A Divina Comédia'];
+// const nomeDoLivro = "Dom Quixote";
+
+// const posicaoDoLivro = livros.indexOf(nomeDoLivro);
+
+// if (posicaoDoLivro !== -1) {
+//   console.log(`O livro está na posição ${posicaoDoLivro + 1}`);
+// } else {
+//   console.log("Livro não encontrado na estante.");
+// }
