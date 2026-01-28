96e3c1b774fae7ed7ba1618ddb501d405ce66767
Cleyton Santos <135157379+cleytonsantos7@users.noreply.github.com>
2026-01-28 09:55:23 -0300

    Adição de exemplos do metodo sort

---
+++ metodosArray3/sort.js
@@ -0,0 +1,17 @@
+// const array = [2, 4, 1, 20, 3, 43];
+
+// array.sort();
+
+// const array1 = [2, 4, 1, 20, 3, 43];
+
+// array1.sort((a, b) => {
+//     return a - b;
+// });
+
+// console.log(array, array1);
+
+const numeros = [1, 30, 4, 6, 80, 34, 100, 256, 1000];
+
+numeros.sort();
+
+console.log(numeros);
