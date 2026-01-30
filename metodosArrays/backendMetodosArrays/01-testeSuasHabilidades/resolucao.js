1982258d4c34ce443e0bb394d6c9cdbfc63e5580
Cleyton Santos <135157379+cleytonsantos7@users.noreply.github.com>
2026-01-29 21:01:40 -0300

    Adicionado a resolução do exercicio 1, da pasta backendMetodosArrays

---
+++ metodosArrays/backendMetodosArrays/01-testeSuasHabilidades/resolucao.js
@@ -0,0 +1,11 @@
+const frutas = ['Banana', 'Maçã', 'Abacaxi', 'Pêra', 'Uva'];
+
+const arrayInverso = frutas.reverse().join(', ');
+
+console.log(arrayInverso);
+
+frutas.shift();
+frutas.pop();
+frutas.push('Melão')
+
+console.log(frutas);
