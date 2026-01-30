ffb988f6a2b7030ff610dd7f9d65e247414c0bbd
Cleyton Santos <135157379+cleytonsantos7@users.noreply.github.com>
2026-01-29 21:31:51 -0300

    Adicionado a resolução do exercicio 5, da pasta backendMetodosArrays

---
+++ metodosArrays/backendMetodosArrays/05-filaDeAtendimento-2.0/resolucao.js
@@ -0,0 +1,20 @@
+const pacientes = ['José', 'Pedro', 'Maria', 'João', 'Ana', 'Bárbara', 'Joana'];
+
+const agendarPaciente = (lista, nomePaciente) => {
+    lista.push(nomePaciente);
+    console.log(lista.join(', '));
+}
+
+const atenderPaciente = (lista) => {
+    lista.shift();
+    console.log(lista.join(', '));
+}
+
+const cancelarAtendimento = (lista, nomePaciente) => {
+    lista.splice(lista.indexOf(nomePaciente), 1);
+    console.log(lista.join(', '));
+}
+
+agendarPaciente(pacientes, 'Guido');
+atenderPaciente(pacientes);
+cancelarAtendimento(pacientes, 'João');
