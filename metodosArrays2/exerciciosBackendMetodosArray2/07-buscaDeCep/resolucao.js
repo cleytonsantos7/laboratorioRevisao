6d60894ee517324f2b88cc2a4095b82f68a79f2a
Cleyton Santos <135157379+cleytonsantos7@users.noreply.github.com>
2026-01-29 22:33:24 -0300

    Modificado a resolução do exercicio 7, da pasta exerciciosBackendMetodosArrays2, pois o vscode não estava aceitando os ceps com o tipo number por conta dos 0 a esquerda

--- metodosArrays2/exerciciosBackendMetodosArray2/07-buscaDeCep/resolucao.js
+++ metodosArrays2/exerciciosBackendMetodosArray2/07-buscaDeCep/resolucao.js
@@ -1,9 +1,9 @@
 function buscarEndereco(cep) {
     const enderecos = [
-        { cep: 00111222, rua: "Rua dos Artistas" },
-        { cep: 00111333, rua: "Rua Augusta" },
-        { cep: 00222444, rua: "Avenida Paralela" },
-        { cep: 11222333, rua: "Rua Carlos Gomes" },
+        { cep: '00111222', rua: "Rua dos Artistas" },
+        { cep: '00111333', rua: "Rua Augusta" },
+        { cep: '00222444', rua: "Avenida Paralela" },
+        { cep: '11222333', rua: "Rua Carlos Gomes" },
     ];
 
     const enderecoEncontrado = enderecos.find(function (valor) {
@@ -13,4 +13,4 @@ function buscarEndereco(cep) {
     console.log(enderecoEncontrado.rua);
 }
 
-buscarEndereco(00222444);
+buscarEndereco('00222444');
