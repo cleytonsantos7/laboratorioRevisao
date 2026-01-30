208632deaa446a00889b7483f17f7ed0bbe0aa07
Cleyton Santos <135157379+cleytonsantos7@users.noreply.github.com>
2026-01-29 21:13:53 -0300

    Adicionado a resolução do exercicio 3, da pasta backendMetodosArrays, e adicionado o join na minha resposta, para corrigir um erro posi não estava imprimido o esperado

--- metodosArrays/backendMetodosArrays/03-gruposDeCarros/index.js
+++ metodosArrays/backendMetodosArrays/03-gruposDeCarros/index.js
@@ -4,7 +4,7 @@ const posicao = 3;
 function declararPosicao(nomes, posicao) {
     const nomes3posicoes = nomes.slice(posicao, posicao + 3);
 
-    console.log(nomes3posicoes)
+    console.log(nomes3posicoes.join(' - '))
 }
 
 declararPosicao(nomes, posicao)
