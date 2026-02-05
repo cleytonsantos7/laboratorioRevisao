143301449962ad7b9de3c3fbddd23b0f1bbfbed3
Cleyton Santos <135157379+cleytonsantos7@users.noreply.github.com>
2026-02-05 19:39:31 -0300

    Adição dos controladores do servidor

--- Modulo-02/rotasIntermediariosEControladores/configuracao-inicial-do-servidor/src/index.js
+++ Modulo-02/rotasIntermediariosEControladores/configuracao-inicial-do-servidor/src/index.js
@@ -1,32 +1,11 @@
 const express = require('express');
+const { filtrarProfessores, encontrarUmProfessor } = require('./controladores/professores')
 
 const app = express();
 
-const professores = [
-    { id: 1, nome: 'Guido', stack: 'Backend' },
-    { id: 2, nome: 'Dani', stack: 'Frontend' },
-    { id: 3, nome: 'Diego', stack: 'Frontend' },
-    { id: 4, nome: 'Vidal', stack: 'Backend' }
-];
 
-app.get('/professores', (req, res) => {
-    const { stack } = req.query;
-    let resultado = professores;
+app.get('/professores', filtrarProfessores);
 
-    if (stack) {
-        resultado = professores.filter((professor) => {
-            return professor.stack === stack
-        });
-    }
-
-    res.send(resultado)
-});
-
-app.get('/professores/:id', (req, res) => {
-    const professorEncontrado = professores.find((professor) => {
-        return professor.id === Number(req.params.id)
-    })
-    res.send(professorEncontrado)
-});
+app.get('/professores/:id', encontrarUmProfessor);
 
 app.listen(3000);
