19f54bc00122c064be10fd34c6190f48c53c2183
Cleyton Santos <135157379+cleytonsantos7@users.noreply.github.com>
2026-02-09 18:45:50 -0300

    Criado o controlador atualizarStatusInstrutor, adicionada a rota patch, e corrigido o status da mensagem de resposta da rota put, de 203 para 204

--- Modulo-02/API-REST/src/controladores/instrutores.js
+++ Modulo-02/API-REST/src/controladores/instrutores.js
@@ -54,10 +54,6 @@ const atualizarInstrutor = (req, res) => {
         return res.status(400).json({ mensagem: 'O email é obrigatório' });
     }
 
-    if (!status) {
-        return res.status(400).json({ mensagem: 'O status é obrigatório' });
-    }
-
     const instrutor = instrutores.find((instrutor) => {
         return instrutor.id === Number(id);
     });
@@ -70,12 +66,29 @@ const atualizarInstrutor = (req, res) => {
     instrutor.email = email;
     instrutor.status = status;
 
-    return res.status(203).send();
+    return res.status(204).send();
+};
+
+const atualizarStatusInstrutor = (req, res) => {
+    const { id } = req.params;
+    const { status } = req.body;
+    
+    const instrutor = instrutores.find((instrutor) => {
+        return instrutor.id === Number(id);
+    });
+
+    if(!instrutor) {
+        return res.status(404).json({ mensagem: 'Instrutor não encontrado.' });
+    }
+
+    instrutor.status = status;
+    return res.status(204).send();
 };
 
 module.exports = {
     listarInstrutores,
     obterInstrutor,
     cadastrarInstrutor,
-    atualizarInstrutor
+    atualizarInstrutor,
+    atualizarStatusInstrutor
 }
