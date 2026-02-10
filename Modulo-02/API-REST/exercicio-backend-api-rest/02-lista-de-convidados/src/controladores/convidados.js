df07c39382df740276151305278bf608e2c499c2
Cleyton Santos <135157379+cleytonsantos7@users.noreply.github.com>
2026-02-10 10:24:07 -0300

    Adicionado a possibilidade de consultar o nome de um convidado na rota get, e adicionado o controlador para adicionar um convidado e criado a rota post

--- Modulo-02/API-REST/exercicio-backend-api-rest/02-lista-de-convidados/src/controladores/convidados.js
+++ Modulo-02/API-REST/exercicio-backend-api-rest/02-lista-de-convidados/src/controladores/convidados.js
@@ -1,9 +1,37 @@
 const convidados = require('../dados/convidados');
 
 const listarConvidados = (req, res) => {
+    const { nome } = req.query;
+
+    if (nome) {
+    if (convidados.includes(nome)) {
+        return res.json({ "mensagem": "Convidado presente." });
+    }
+    return res.json({ "mensagem": "O convidado buscado não está presente na lista." });
+    }
+
     return res.status(200).json(convidados);
 };
 
+const adicionarConvidado = (req, res) => {
+    const { nome } = req.body;
+
+    if (!nome || nome.trim() === "") {
+        return res.status(400).json({ "mensagem": "O nome do convidado deve ser informado." });
+    }
+
+    if (convidados.includes(nome)) {
+        return res.status(400).json({
+        "mensagem": "O nome do convidado a ser adicionado já existe na lista. Caso queria adicionar outro convidado de mesmo nome, favor fornecer o sobrenome também."
+        });
+    }
+
+    convidados.push(nome);
+
+    return res.status(201).json({ "mensagem": "Convidado adicionado." });
+}; 
+
 module.exports = {
-    listarConvidados
-}
+    listarConvidados,
+    adicionarConvidado
+};
--- Modulo-02/API-REST/exercicio-backend-api-rest/02-lista-de-convidados/src/roteador.js
+++ Modulo-02/API-REST/exercicio-backend-api-rest/02-lista-de-convidados/src/roteador.js
@@ -1,9 +1,10 @@
 const express = require('express');
-const { listarConvidados } = require('./controladores/convidados');
+const { listarConvidados, adicionarConvidado } = require('./controladores/convidados');
 const convidados = require('./dados/convidados');
 
 const rotas = express.Router();
 
 rotas.get('/convidados', listarConvidados);
+rotas.post('/convidados', adicionarConvidado);
 
 module.exports = rotas;
