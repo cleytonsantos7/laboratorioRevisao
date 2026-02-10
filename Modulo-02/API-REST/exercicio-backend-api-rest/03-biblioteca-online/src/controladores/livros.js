05dc567508a354326c8e19ceec0916bc24555238
Cleyton Santos <135157379+cleytonsantos7@users.noreply.github.com>
2026-02-10 20:18:08 -0300

    Adicionado controlador para substituir um livro, e também criada a rota put

--- Modulo-02/API-REST/exercicio-backend-api-rest/03-biblioteca-online/src/controladores/livros.js
+++ Modulo-02/API-REST/exercicio-backend-api-rest/03-biblioteca-online/src/controladores/livros.js
@@ -54,8 +54,49 @@ const adicionarLivro = (req, res) => {
     return res.status(201).send();
 };
 
+const substituirLivro = (req, res) => {
+    const { titulo, autor, ano, numPaginas } = req.body;
+    const { id } = req.params;
+
+    if (isNaN(Number(id))) {
+        return res.status(400).json({ mensagem: 'O valor do parâmetro ID da URL não é um número válido.'});
+    }
+
+    if (!titulo) {
+        return res.status(400).json({ mensagem: 'O titulo deve ser informado.'});
+    }
+
+    if (!autor) {
+        return res.status(400).json({ mensagem: 'O autor deve ser informado.'});
+    }
+
+    if (!ano) {
+        return res.status(400).json({ mensagem: 'O ano deve ser informado.'});
+    }
+
+    if (!numPaginas) {
+        return res.status(400).json({ mensagem: 'O número de páginas deve ser informado.'});
+    }
+
+    const livroEncontrado = livros.find((livro) => {
+        return livro.id === Number(id);
+    });
+
+    if (!livroEncontrado) {
+        return res.status(404).json({ mensagem: 'O livro não foi encontrado.' });
+    }
+
+    livroEncontrado.titulo = titulo;
+    livroEncontrado.autor = autor;
+    livroEncontrado.ano = ano;
+    livroEncontrado.numPaginas = numPaginas;
+
+    return res.status(204).send();
+}
+
 module.exports = {
     listarLivros,
     encotrarLivro,
-    adicionarLivro
+    adicionarLivro,
+    substituirLivro
 }
--- Modulo-02/API-REST/exercicio-backend-api-rest/03-biblioteca-online/src/roteador.js
+++ Modulo-02/API-REST/exercicio-backend-api-rest/03-biblioteca-online/src/roteador.js
@@ -1,10 +1,11 @@
 const express = require('express');
-let { listarLivros, encotrarLivro, adicionarLivro } = require('./controladores/livros');
+let { listarLivros, encotrarLivro, adicionarLivro, substituirLivro } = require('./controladores/livros');
 
 const rotas = express.Router();
 
 rotas.get('/livros', listarLivros);
 rotas.get('/:id', encotrarLivro);
 rotas.post('/livros', adicionarLivro);
+rotas.put('/:id', substituirLivro);
 
 module.exports = rotas;
