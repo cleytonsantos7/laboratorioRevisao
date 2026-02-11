b48fe5179086757220de39f8160083a96c465c92
Cleyton Santos <135157379+cleytonsantos7@users.noreply.github.com>
2026-02-11 17:44:35 -0300

    Adicionado o controlador obterProduto, assim como também a sua rota

--- Modulo-02/programacao-assincrona/exercicios-backend-programacao-assincrona/01-calculo-de-frete/src/controladores/produtos.js
+++ Modulo-02/programacao-assincrona/exercicios-backend-programacao-assincrona/01-calculo-de-frete/src/controladores/produtos.js
@@ -1,9 +1,24 @@
 const produtos = require('../bancodedados/produtos');
 
-const listarProdutos = (req, res) => {
+const listarProdutos = async (req, res) => {
     return res.status(200).json(produtos);
 }; 
 
+const obterProduto = async (req, res) => {
+    const { idProduto } = req.params;
+
+    const produto = produtos.find((produto) => {
+        return produto.id === Number(idProduto);
+    });
+
+    if (!produto) {
+        return res.status(404).json({ mensagem: 'O produto não existe.'});
+    }
+
+    return res.status(200).json(produto);
+};
+
 module.exports = {
-    listarProdutos
+    listarProdutos,
+    obterProduto
 };
--- Modulo-02/programacao-assincrona/exercicios-backend-programacao-assincrona/01-calculo-de-frete/src/roteador.js
+++ Modulo-02/programacao-assincrona/exercicios-backend-programacao-assincrona/01-calculo-de-frete/src/roteador.js
@@ -1,8 +1,9 @@
 const express = require('express');
-const { listarProdutos } = require('./controladores/produtos')
+const { listarProdutos, obterProduto } = require('./controladores/produtos')
 
 const rotas = express.Router();
 
 rotas.get('/produtos', listarProdutos);
+rotas.get('/produtos/:idProduto', obterProduto);
 
 module.exports = rotas;
