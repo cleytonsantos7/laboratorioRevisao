5fe4a60750257937e2cc98d54215d25e69cfbdfc
Cleyton Santos <135157379+cleytonsantos7@users.noreply.github.com>
2026-02-13 11:11:18 -0300

    Adicionada a solução do exercicio 02, conforme enunciado

--- Modulo-02/leitura-e-escrita-de-arquivos/exercicios-backend-leitura-escrita-arquivos/02-tratamento-de-excecoes-na-api-pokemon/src/controladores/pokemons.js
+++ Modulo-02/leitura-e-escrita-de-arquivos/exercicios-backend-leitura-escrita-arquivos/02-tratamento-de-excecoes-na-api-pokemon/src/controladores/pokemons.js
@@ -1,33 +1,56 @@
-const { listarPokemons, detalharPokemon} = require('utils-playground');
+const { listarPokemons, detalharPokemon } = require("utils-playground");
 
 const listagemDePokemons = async (req, res) => {
-    const { pagina } = req.query;
+  const { pagina } = req.query;
 
+  try {
     const pokemons = await listarPokemons(pagina);
 
     return res.status(200).json(pokemons.results);
+  } catch (erro) {
+    console.log(erro.message);
+
+    return res.status(500).json({
+      mensagem: "Erro interno no servidor. Tente novamente mais tarde.",
+    });
+  }
 };
 
 const descricaoPokemon = async (req, res) => {
-    const { idOuNome } = req.params;
+  const { idOuNome } = req.params;
 
-    const { 
-        id, 
-        name, 
-        height, 
-        weight, 
-        base_experience, 
-        forms, 
-        abilities, 
-        species 
+  try {
+    const {
+      id,
+      name,
+      height,
+      weight,
+      base_experience,
+      forms,
+      abilities,
+      species,
     } = await detalharPokemon(idOuNome);
 
-    return res.status(200).json({ 
-        id, name, height, weight, base_experience, forms, abilities, species 
+    return res.status(200).json({
+      id,
+      name,
+      height,
+      weight,
+      base_experience,
+      forms,
+      abilities,
+      species,
     });
+  } catch (erro) {
+    console.log(erro.message);
+
+    return res.status(500).json({
+      mensagem: "Erro interno no servidor. Tente novamente mais tarde.",
+    });
+  }
 };
 
 module.exports = {
-    listagemDePokemons,
-    descricaoPokemon
+  listagemDePokemons,
+  descricaoPokemon,
 };
