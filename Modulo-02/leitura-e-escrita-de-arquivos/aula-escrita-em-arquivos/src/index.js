04b8902dc21eda631aad0d32362368f0616230c2
Cleyton Santos <135157379+cleytonsantos7@users.noreply.github.com>
2026-02-12 23:40:14 -0300

    Criação da pasta aula-try-catch-finally, e adição de exemplo

--- Modulo-02/leitura-e-escrita-de-arquivos/aula-escrita-em-arquivos/src/index.js
+++ Modulo-02/leitura-e-escrita-de-arquivos/aula-escrita-em-arquivos/src/index.js
@@ -1,30 +1,30 @@
-const express = require('express');
-const fs = require('fs/promises');
+const express = require("express");
+const fs = require("fs/promises");
 
 const app = express();
 
 app.use(express.json());
 
-app.get('/', async (req, res) => {
-    const texto = 'tudo bem?'
-    await fs.writeFile('./src/b.txt', texto);
-    res.json('ok')
+app.get("/", async (req, res) => {
+  const texto = "tudo bem?";
+  await fs.writeFile("./src/b.txt", texto);
+  res.json("ok");
 });
 
-app.post('/', async (req, res) => {
-    const { nome, idade } = req.body;
+app.post("/", async (req, res) => {
+  const { nome, idade } = req.body;
 
-    const teste = await fs.readFile('./src/usuarios.json');
+  const teste = await fs.readFile("./src/usuarios.json");
 
-    const pessoas = JSON.parse(teste);
+  const pessoas = JSON.parse(teste);
 
-    pessoas.push({ nome, idade });
+  pessoas.push({ nome, idade });
 
-    const pessoasStringfy = JSON.stringify(pessoas);
+  const pessoasStringfy = JSON.stringify(pessoas);
 
-    await fs.writeFile('./src/usuarios.json', pessoasStringfy)
+  await fs.writeFile("./src/usuarios.json", pessoasStringfy);
 
-    return res.json('Pessoa cadastrada com sucesso!')
+  return res.json("Pessoa cadastrada com sucesso!");
 });
 
 app.listen(3000);
