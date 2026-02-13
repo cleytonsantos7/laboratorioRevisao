a1b50c05267b05d0fba93274baa2e5741c8b4778
Cleyton Santos <135157379+cleytonsantos7@users.noreply.github.com>
2026-02-13 11:06:12 -0300

    Adicionada a solução do exercicio 01, conforme enunciado

--- Modulo-02/leitura-e-escrita-de-arquivos/exercicios-backend-leitura-escrita-arquivos/01-tratamento-de-excecoes-na-api-calculo-de-frete/src/controladores/produtos.js
+++ Modulo-02/leitura-e-escrita-de-arquivos/exercicios-backend-leitura-escrita-arquivos/01-tratamento-de-excecoes-na-api-calculo-de-frete/src/controladores/produtos.js
@@ -1,60 +1,84 @@
-const produtos = require('../bancodedados/produtos');
-const { getStateFromZipcode } = require('utils-playground');
+const produtos = require("../bancodedados/produtos");
+const { getStateFromZipcode } = require("utils-playground");
 
 const listarProdutos = async (req, res) => {
+  try {
     return res.status(200).json(produtos);
-}; 
+  } catch (erro) {
+    console.log(erro.message);
+
+    return res.status(500).json({
+      mensagem: "Erro interno no servidor. Tente novamente mais tarde.",
+    });
+  }
+};
 
 const obterProduto = async (req, res) => {
-    const { idProduto } = req.params;
+  const { idProduto } = req.params;
 
+  try {
     const produto = produtos.find((produto) => {
-        return produto.id === Number(idProduto);
+      return produto.id === Number(idProduto);
     });
 
     if (!produto) {
-        return res.status(404).json({ mensagem: 'O produto não existe.'});
+      return res.status(404).json({ mensagem: "O produto não existe." });
     }
 
     return res.status(200).json(produto);
+  } catch (erro) {
+    console.log(erro.message);
+
+    return res.status(500).json({
+      mensagem: "Erro interno no servidor. Tente novamente mais tarde.",
+    });
+  }
 };
 
 const calcularFrete = async (req, res) => {
-    const { idProduto, cep } = req.params;
-    
+  const { idProduto, cep } = req.params;
+
+  try {
     const produto = produtos.find((produto) => {
-        return produto.id === Number(idProduto);
+      return produto.id === Number(idProduto);
     });
 
     if (!produto) {
-        return res.status(404).json({ mensagem: 'O produto não existe.'});
+      return res.status(404).json({ mensagem: "O produto não existe." });
     }
 
     const estado = await getStateFromZipcode(cep);
 
-    let valorPorcentagem =  0.12;
+    let valorPorcentagem = 0.12;
 
-    if (['BA', 'SE', 'AL', 'PE', 'PB'].includes(estado)){
-        valorPorcentagem = 0.10;
-    };
+    if (["BA", "SE", "AL", "PE", "PB"].includes(estado)) {
+      valorPorcentagem = 0.1;
+    }
 
-    if (['SP', 'RJ'].includes(estado)){
-        valorPorcentagem = 0.15;
-    };
+    if (["SP", "RJ"].includes(estado)) {
+      valorPorcentagem = 0.15;
+    }
 
     const valorFrete = produto.valor * valorPorcentagem;
 
     const resultado = {
-        produto,
-        estado,
-        valorFrete
+      produto,
+      estado,
+      valorFrete,
     };
 
     return res.status(200).json(resultado);
+  } catch (erro) {
+    console.log(erro.message);
+
+    return res.status(500).json({
+      mensagem: "Erro interno no servidor. Tente novamente mais tarde.",
+    });
+  }
 };
 
 module.exports = {
-    listarProdutos,
-    obterProduto,
-    calcularFrete
+  listarProdutos,
+  obterProduto,
+  calcularFrete,
 };
