4c41f4e39ff9239138e153b3fbede0f0e105fc17
Cleyton Santos <135157379+cleytonsantos7@users.noreply.github.com>
2026-04-06 16:46:23 -0300

    adicionada a resolução do exercicio

---
+++ Modulo-03/aula-modelagem-de-dados/exercicios-backend-modelagem-de-dados/01/query.sql
@@ -0,0 +1,113 @@
+create database ecommerce;
+
+CREATE TABLE categorias (
+  id SERIAL PRIMARY KEY,
+  nome VARCHAR(50)
+);
+
+CREATE TABLE produtos (
+  id SERIAL PRIMARY KEY,
+  nome VARCHAR(100),
+  descricao TEXT,
+  preco INTEGER,
+  quantidade_em_estoque INTEGER,
+  categoria_id INTEGER,
+  FOREIGN KEY (categoria_id) REFERENCES categorias(id)
+);
+
+create table clientes (
+  cpf char(11) unique,
+  nome varchar(150)
+);
+
+create table vendedores (
+  cpf char(11) unique,
+  nome varchar(150)
+);
+
+create table pedidos (
+  id serial primary key,
+  valor integer,
+  cliente_cpf CHAR(11) REFERENCES clientes(cpf),
+  vendedor_cpf CHAR(11) REFERENCES vendedores(cpf)
+);
+
+create table itens_do_pedido (
+  id serial primary key,
+  pedido_id integer references pedidos (id),
+  quantidade integer,
+  produto_id integer references produtos (id)
+);
+
+INSERT INTO categorias (nome) VALUES 
+('frutas'), ('verduras'), ('massas'), ('bebidas'), ('utilidades');
+
+INSERT INTO produtos (nome, descricao, preco, quantidade_em_estoque, categoria_id) VALUES
+('Mamão', 'Rico em vitamina A, potássio e vitamina C', 300, 123, 1),
+('Maça', 'Fonte de potássio e fibras.', 90, 34, 1),
+('Cebola', 'Rico em quercetina, antocianinas, vitaminas do complexo B, C.', 50, 76, 2),
+('Abacate', 'NÃO CONTÉM GLÚTEN.', 150, 64, 1),
+('Tomate', 'Rico em vitaminas A, B e C.', 125, 88, 2),
+('Acelga', 'NÃO CONTÉM GLÚTEN.', 235, 13, 2),
+('Macarrão parafuso', 'Sêmola de trigo enriquecida com ferro e ácido fólico, ovos e corantes naturais', 690, 5, 3),
+('Massa para lasanha', 'Uma reunião de família precisa ter comida boa e muita alegria.', 875, 19, 3),
+('Refrigerante coca cola lata', 'Sabor original', 350, 189, 4),
+('Refrigerante Pepsi 2l', 'NÃO CONTÉM GLÚTEN. NÃO ALCOÓLICO.', 700, 12, 4),
+('Cerveja Heineken 600ml', 'Heineken é uma cerveja lager Puro Malte, refrescante e de cor amarelo-dourado', 1200, 500, 4),
+('Agua mineral sem gás', 'Smartwater é água adicionado de sais mineirais livre de sódio e com pH neutro.', 130, 478, 4),
+('Vassoura', 'Pigmento, matéria sintética e metal.', 2350, 30, 5),
+('Saco para lixo', 'Reforçado para garantir mais segurança', 1340, 90, 5),
+('Escova dental', 'Faça uma limpeza profunda com a tecnologia inovadora', 1000, 44, 5),
+('Balde para lixo 50l', 'Possui tampa e fabricado com material reciclado', 2290, 55, 5),
+('Manga', 'Rico em Vitamina A, potássio e vitamina C', 198, 176, 1),
+('Uva', 'NÃO CONTÉM GLÚTEN.', 420, 90, 1);
+
+INSERT INTO clientes (cpf, nome) VALUES
+('80371350042', 'José Augusto Silva'),
+('67642869061', 'Antonio Oliveira'),
+('63193310034', 'Ana Rodrigues'),
+('75670505018', 'Maria da Conceição');
+
+INSERT INTO vendedores (cpf, nome) VALUES
+('82539841031', 'Rodrigo Sampaio'),
+('23262546003', 'Beatriz Souza Santos'),
+('28007155023', 'Carlos Eduardo');
+
+INSERT INTO pedidos (valor, cliente_cpf, vendedor_cpf) VALUES (8440, '80371350042', '28007155023');
+INSERT INTO itens_do_pedido (pedido_id, quantidade, produto_id) VALUES (1, 1, 1), (1, 1, 10), (1, 6, 11), (1, 1, 15), (1, 5, 2);
+UPDATE produtos SET quantidade_em_estoque = quantidade_em_estoque - 1 WHERE id = 1;
+UPDATE produtos SET quantidade_em_estoque = quantidade_em_estoque - 1 WHERE id = 10;
+UPDATE produtos SET quantidade_em_estoque = quantidade_em_estoque - 6 WHERE id = 11;
+UPDATE produtos SET quantidade_em_estoque = quantidade_em_estoque - 1 WHERE id = 15;
+UPDATE produtos SET quantidade_em_estoque = quantidade_em_estoque - 5 WHERE id = 2;
+
+INSERT INTO pedidos (valor, cliente_cpf, vendedor_cpf) VALUES (5460, '63193310034', '23262546003');
+INSERT INTO itens_do_pedido (pedido_id, quantidade, produto_id) VALUES (2, 10, 17), (2, 3, 18), (2, 5, 1), (2, 10, 5), (2, 2, 6);
+UPDATE produtos SET quantidade_em_estoque = quantidade_em_estoque - 10 WHERE id = 17;
+UPDATE produtos SET quantidade_em_estoque = quantidade_em_estoque - 3 WHERE id = 18;
+UPDATE produtos SET quantidade_em_estoque = quantidade_em_estoque - 5 WHERE id = 1;
+UPDATE produtos SET quantidade_em_estoque = quantidade_em_estoque - 10 WHERE id = 5;
+UPDATE produtos SET quantidade_em_estoque = quantidade_em_estoque - 2 WHERE id = 6;
+
+INSERT INTO pedidos (valor, cliente_cpf, vendedor_cpf) VALUES (4120, '75670505018', '23262546003');
+INSERT INTO itens_do_pedido (pedido_id, quantidade, produto_id) VALUES (3, 1, 13), (3, 6, 12), (3, 5, 17);
+UPDATE produtos SET quantidade_em_estoque = quantidade_em_estoque - 1 WHERE id = 13;
+UPDATE produtos SET quantidade_em_estoque = quantidade_em_estoque - 6 WHERE id = 12;
+UPDATE produtos SET quantidade_em_estoque = quantidade_em_estoque - 5 WHERE id = 17;
+
+INSERT INTO pedidos (valor, cliente_cpf, vendedor_cpf) VALUES (7506, '75670505018', '82539841031');
+INSERT INTO itens_do_pedido (pedido_id, quantidade, produto_id) VALUES (4, 1, 16), (4, 6, 18), (4, 1, 7), (4, 3, 1), (4, 20, 5), (4, 2, 6);
+UPDATE produtos SET quantidade_em_estoque = quantidade_em_estoque - 1 WHERE id = 16;
+UPDATE produtos SET quantidade_em_estoque = quantidade_em_estoque - 6 WHERE id = 18;
+UPDATE produtos SET quantidade_em_estoque = quantidade_em_estoque - 1 WHERE id = 7;
+UPDATE produtos SET quantidade_em_estoque = quantidade_em_estoque - 3 WHERE id = 1;
+UPDATE produtos SET quantidade_em_estoque = quantidade_em_estoque - 20 WHERE id = 5;
+UPDATE produtos SET quantidade_em_estoque = quantidade_em_estoque - 2 WHERE id = 6;
+
+INSERT INTO pedidos (valor, cliente_cpf, vendedor_cpf) VALUES (8229, '67642869061', '82539841031');
+INSERT INTO itens_do_pedido (pedido_id, quantidade, produto_id) VALUES (5, 8, 18), (5, 1, 8), (5, 3, 17), (5, 8, 5), (5, 2, 11);
+UPDATE produtos SET quantidade_em_estoque = quantidade_em_estoque - 8 WHERE id = 18;
+UPDATE produtos SET quantidade_em_estoque = quantidade_em_estoque - 1 WHERE id = 8;
+UPDATE produtos SET quantidade_em_estoque = quantidade_em_estoque - 3 WHERE id = 17;
+UPDATE produtos SET quantidade_em_estoque = quantidade_em_estoque - 8 WHERE id = 5;
+UPDATE produtos SET quantidade_em_estoque = quantidade_em_estoque - 2 WHERE id = 11;
