81b9430b43798de12e39bd5aa136b32c2ebd5c77
Cleyton Santos <135157379+cleytonsantos7@users.noreply.github.com>
2026-04-07 16:13:12 -0300

    Configurada uma conexão ao banco de dados através de um Client

--- Modulo-03/aula-conexao-nodeJs-com-postgreSQL/index.js
+++ Modulo-03/aula-conexao-nodeJs-com-postgreSQL/index.js
@@ -1,11 +1,30 @@
 const express = require("express");
+const { Client } = require("pg");
 
 const app = express();
 
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  return res.json("Tudo certo");
+app.get("/", async (req, res) => {
+  const client = new Client({
+    host: "localhost",
+    port: 5432,
+    user: "postgres",
+    password: "123456",
+    database: "aula_conexao_node_pg",
+  });
+
+  try {
+    await client.connect();
+
+    const resultado = await client.query("select * from empresas");
+
+    await client.end();
+
+    return res.json(resultado);
+  } catch (error) {
+    console.log(error.message);
+  }
 });
 
 app.listen(3000);
