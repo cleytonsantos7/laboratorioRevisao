5e4fe44af52625aab6b230cca70b37a0af7cf0f7
Cleyton Santos <135157379+cleytonsantos7@users.noreply.github.com>
2026-04-07 17:01:20 -0300

    Alterado a conexão de um Client, para uma Pool

--- Modulo-03/aula-conexao-nodeJs-com-postgreSQL/index.js
+++ Modulo-03/aula-conexao-nodeJs-com-postgreSQL/index.js
@@ -1,25 +1,13 @@
 const express = require("express");
-const { Client } = require("pg");
+const pool = require("./conexao");
 
 const app = express();
 
 app.use(express.json());
 
 app.get("/", async (req, res) => {
-  const client = new Client({
-    host: "localhost",
-    port: 5432,
-    user: "postgres",
-    password: "123456",
-    database: "aula_conexao_node_pg",
-  });
-
   try {
-    await client.connect();
-
-    const resultado = await client.query("select * from empresas");
-
-    await client.end();
+    const resultado = await pool.query("select * from empresas");
 
     return res.json(resultado);
   } catch (error) {
