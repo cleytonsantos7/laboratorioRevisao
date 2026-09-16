a4bf3b60e7b2d75a0da38d2b102e6d19a977b217
Cleyton Santos <135157379+cleytonsantos7@users.noreply.github.com>
2026-09-16 16:05:09 -0300

    Adicionado um exemplo de paginação dos dados

--- Modulo-03/aula-conexao-nodeJs-com-postgreSQL/index.js
+++ Modulo-03/aula-conexao-nodeJs-com-postgreSQL/index.js
@@ -6,10 +6,26 @@ const app = express();
 app.use(express.json());
 
 app.get("/", async (req, res) => {
-  try {
-    const resultado = await pool.query("select * from empresas");
+  // const { id } = req.params;
 
-    return res.json(resultado);
+  const { pagina, porPagina } = req.query;
+  try {
+    // const query = "select * from empresas where id = $1";
+    // const params = [id];
+
+    const query = "select * from pessoas order by id asc limit $1 offset $2";
+
+    const offset = pagina === 1 ? 0 : (pagina - 1) * porPagina;
+
+    const resultado = await pool.query(query, [porPagina, offset]);
+
+    const result = {
+      pagina,
+      porPagina,
+      total: resultado.rowCount,
+      registros: resultado.rows,
+    };
+    return res.json(result);
   } catch (error) {
     console.log(error.message);
   }
