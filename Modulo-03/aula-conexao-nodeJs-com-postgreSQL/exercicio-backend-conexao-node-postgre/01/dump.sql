c064ede2c3fa699b7d3e9e548a03b5b2c2e23f00
Cleyton Santos <135157379+cleytonsantos7@users.noreply.github.com>
2026-09-21 15:14:00 -0300

    adicionada a query de criação da tabela livros, ao arquivo dump.sql

--- Modulo-03/aula-conexao-nodeJs-com-postgreSQL/exercicio-backend-conexao-node-postgre/01/dump.sql
+++ Modulo-03/aula-conexao-nodeJs-com-postgreSQL/exercicio-backend-conexao-node-postgre/01/dump.sql
@@ -6,3 +6,11 @@ create table "autores" (
   idade integer NULL
 );
 
+create table "livros" (
+  id SERIAL PRIMARY KEY,
+  nome varchar(255) NOT NULL,
+  genero varchar (255) default null,
+  editora varchar (255) default null,
+  data_publicacao date,
+  autor_id integer references autores(id)
+);
