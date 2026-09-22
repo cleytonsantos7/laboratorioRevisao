74f93ae33ea2cc1b1d43535100c5614b755a3db4
Cleyton Santos <135157379+cleytonsantos7@users.noreply.github.com>
2026-09-22 15:24:56 -0300

    Instalada a biblioteca JWT, e adicionado a geração de token a função de login

--- Modulo-03/aula-autenticacao-e-criptografia/src/controladores/usuarios.js
+++ Modulo-03/aula-autenticacao-e-criptografia/src/controladores/usuarios.js
@@ -1,5 +1,7 @@
 const pool = require("../conexao");
 const bycript = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const senhaJwt = require("../senhaJwt");
 
 const cadastrarUsuario = async (req, res) => {
   const { nome, email, senha } = req.body;
@@ -41,8 +43,13 @@ const login = async (req, res) => {
     if (!senhaValida) {
       return res.status(400).json({ message: "Email ou senha invalida" });
     }
+    const token = jwt.sign({ id: usuario.rows[0].id }, senhaJwt, {
+      expiresIn: "8h",
+    });
 
-    return res.status(200).json("usuario autenticado");
+    const { senha: _, ...usuarioLogado } = usuario.rows[0];
+
+    return res.status(200).json({ usuario: usuarioLogado, token });
   } catch (error) {
     return res.status(500).json({ message: "Erro interno do servidor" });
   }
