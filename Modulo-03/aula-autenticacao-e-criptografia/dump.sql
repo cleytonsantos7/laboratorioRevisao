09467a260e3136b39e3270173fe34aafbc0f4025
Cleyton Santos <135157379+cleytonsantos7@users.noreply.github.com>
2026-09-22 12:16:01 -0300

    Adicionado a query de criação da tabela usuarios

--- Modulo-03/aula-autenticacao-e-criptografia/dump.sql
+++ Modulo-03/aula-autenticacao-e-criptografia/dump.sql
@@ -6,3 +6,11 @@ create table carros (
     cor text not null,
     descricao text
 );
+
+
+create table usuarios (
+  id serial primary key,
+  nome text not null,
+  email text not null unique,
+  senha text not null
+)
