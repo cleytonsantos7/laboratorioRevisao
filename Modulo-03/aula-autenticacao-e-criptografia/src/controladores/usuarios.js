4c7de63167c0c11700bf8a6e6dfb8c2315f684f5
Cleyton Santos <135157379+cleytonsantos7@users.noreply.github.com>
2026-09-22 14:59:54 -0300

    Criado um contralador de login, assim como sua rota

--- Modulo-03/aula-autenticacao-e-criptografia/src/controladores/usuarios.js
+++ Modulo-03/aula-autenticacao-e-criptografia/src/controladores/usuarios.js
@@ -23,4 +23,29 @@ const cadastrarUsuario = async (req, res) => {
   }
 };
 
-module.exports = { cadastrarUsuario };
+const login = async (req, res) => {
+  const { email, senha } = req.body;
+
+  try {
+    const usuario = await pool.query(
+      "select * from usuarios where email = $1",
+      [email],
+    );
+
+    if (usuario.rowCount < 1) {
+      return res.status(404).json({ message: "Email ou senha invalida" });
+    }
+
+    const senhaValida = await bycript.compare(senha, usuario.rows[0].senha);
+
+    if (!senhaValida) {
+      return res.status(400).json({ message: "Email ou senha invalida" });
+    }
+
+    return res.status(200).json("usuario autenticado");
+  } catch (error) {
+    return res.status(500).json({ message: "Erro interno do servidor" });
+  }
+};
+
+module.exports = { cadastrarUsuario, login };
