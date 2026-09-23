f4e3bb739f8f1d25b16e3d6685b23c33e0b52fcc
Cleyton Santos <135157379+cleytonsantos7@users.noreply.github.com>
2026-09-23 13:27:55 -0300

    Resolvida a 2ª alternativa do exercicio

---
+++ Modulo-03/aula-autenticacao-e-criptografia/exercicio-autenticacao-e-criptografia/01/src/controladores/usuario.js
@@ -0,0 +1,58 @@
+const pool = require("../conexao");
+const bycrpt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const senhaJwt = require("../senhaJwt");
+
+const cadastrarUsuario = async (req, res) => {
+  const { nome, email, senha } = req.body;
+
+  try {
+    const senhaCriptografada = await bycrpt.hash(senha, 10);
+    const novoUsuario = await pool.query(
+      "insert into usuarios (nome, email, senha) values ($1, $2, $3) returning *",
+      [nome, email, senhaCriptografada],
+    );
+
+    return res.status(201).json(novoUsuario.rows[0]);
+  } catch (error) {
+    if (error.code === "23505") {
+      return res
+        .status(400)
+        .json({ message: "O e-mail informado já está cadastrado." });
+    }
+
+    return res.status(500).json({ message: "Erro interno do servidor" });
+  }
+};
+
+const login = async (req, res) => {
+  const { email, senha } = req.body;
+
+  try {
+    const usuario = await pool.query(
+      "select * from usuarios where email = $1",
+      [email],
+    );
+
+    if (usuario.rowCount < 1) {
+      return res.status(404).json({ message: "Email ou senha invalida" });
+    }
+
+    const senhaValida = await bycrpt.compare(senha, usuario.rows[0].senha);
+
+    if (!senhaValida) {
+      return res.status(400).json({ message: "Email ou senha invalida" });
+    }
+    const token = jwt.sign({ id: usuario.rows[0].id }, senhaJwt, {
+      expiresIn: "8h",
+    });
+
+    const { senha: _, ...usuarioLogado } = usuario.rows[0];
+
+    return res.status(200).json({ usuario: usuarioLogado, token });
+  } catch (error) {
+    return res.status(500).json({ message: "Erro interno do servidor" });
+  }
+};
+
+module.exports = { cadastrarUsuario, login };
